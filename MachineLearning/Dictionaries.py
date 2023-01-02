80702593673065ec303839cf5fb99be512748032
DFr3sh <dfr3sh1@protonmail.com>
2023-01-02 15:32:24 +0100

    ajout de Dictionaries

--- MachineLearning/Dictionaries.py
+++ MachineLearning/Dictionaries.py
@@ -4,5 +4,20 @@ traduction = {
     "souris": "mouse",
     "oiseau": "bird"
     }
+"""
+    Créer une fonction trier qui prend comme argument un dictionnaire classeur avec deux associations, 
+    une liste "positif" et une "negatif", censé de ranger un nombre soit positif ou négatif en fonction de son signe 
+ """
 
+classeur = {
+    "positif": [0, 1, 2, 3, 4, 5 , 6],
+    "negatif": [7, 8, 9, 10, 11],
+}
 
+def trier(classeur, nombre):
+    if nombre > 0:
+        classeur['positif'].append(nombre)
+    else:
+        classeur['negatif'].append(nombre)
+        
+    return classeur
